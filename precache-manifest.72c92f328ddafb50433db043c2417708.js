95ff811e3ecd289a2801efd98ea485c80a332231
AkshayGupt <189301019.akshay@gmail.com>
2020-01-18 15:27:44 +0530

    Updates

---
+++ precache-manifest.72c92f328ddafb50433db043c2417708.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3bb1e75e5536b0e64cc3d03644d92c8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "22d6daba37698ba8725a",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "d9469527194c31d0cf32",
+    "url": "/portfolio/static/css/main.999d49de.chunk.css"
+  },
+  {
+    "revision": "22d6daba37698ba8725a",
+    "url": "/portfolio/static/js/2.1855de09.chunk.js"
+  },
+  {
+    "revision": "f111f0e43b20013304c8bd0659a06b92",
+    "url": "/portfolio/static/js/2.1855de09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9469527194c31d0cf32",
+    "url": "/portfolio/static/js/main.bbee437c.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "d59f1dd9a8b2ebae33a7604daac68d33",
+    "url": "/portfolio/static/media/avatar.d59f1dd9.svg"
+  }
+]);
