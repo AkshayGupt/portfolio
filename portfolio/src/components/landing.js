323bfa29cabfeffa06dead0079069126e1c061bc
AkshayGupt <189301019.akshay@gmail.com>
2020-01-18 06:30:28 +0530

    Landing Page

--- portfolio/src/components/landing.js
+++ portfolio/src/components/landing.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-
+import avatar from '../avatar.svg'
 import {Grid,Cell} from 'react-mdl';
 class Landing extends Component{
 
@@ -10,9 +10,19 @@ class Landing extends Component{
             <Grid className="landing-grid">
                 <Cell col={12}>
                     <img 
-                    src="avatar.svg" 
+                    src={avatar} 
                     alt="avatar"
                         />
+                        <div className="text-landing-grid">
+                            <h1>Full Stack Developer</h1>
+                            <hr></hr>
+                            <p>HTML/CSS | JAVASCRIPT | BOOTSTRAP | NodeJS | MongoDB | Express | ReactJS</p>
+                            <div className="social-icons">
+                            <a href="http://google.com" ><i className="fab fa-linkedin"  ></i></a>
+                            <a href="http://google.com" ><i className="fab fa-github"></i></a>
+                             </div>
+                            
+                        </div>
                 </Cell>
             </Grid>
             </div>
