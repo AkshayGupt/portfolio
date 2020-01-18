98d1dff769c4948c38f4c1da6ce43a26b4669f56
AkshayGupt <189301019.akshay@gmail.com>
2020-01-18 17:24:13 +0530

    Updates

---
+++ precache-manifest.dc24541fd44a910c42e93ff2ed252848.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71e9bef6679f3f6c64acb78321b4bbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a201803bd365cf5b5c4a",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "d83d9426577baa429fb4",
+    "url": "/static/css/main.999d49de.chunk.css"
+  },
+  {
+    "revision": "a201803bd365cf5b5c4a",
+    "url": "/static/js/2.e28bce08.chunk.js"
+  },
+  {
+    "revision": "f111f0e43b20013304c8bd0659a06b92",
+    "url": "/static/js/2.e28bce08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d83d9426577baa429fb4",
+    "url": "/static/js/main.9b872a0e.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "d59f1dd9a8b2ebae33a7604daac68d33",
+    "url": "/static/media/avatar.d59f1dd9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2e6760d88e0ea130d09d44591056bebb.js"
+  "/precache-manifest.dc24541fd44a910c42e93ff2ed252848.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
